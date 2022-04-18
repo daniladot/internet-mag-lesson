6ed63df834f5e08e40a9fcae97bb32dda726dd87
Vasya990325 <vasya.arnaut.1999@mail.ru>
2022-04-18 18:38:18 +0300

    slider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import styles from "./App.module.scss";
-import {useDispatch, useSelector} from "react-redux";
-import {fetchAllMenu, fetchMenu} from "./state/MenuReducer/action";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllMenu, fetchMenu } from "./state/MenuReducer/action";
 import MenuItem from "./components/MenuItem/MenuItem.js";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import ProductItem from "./components/ProductItem/ProductItem";
 import MenuPage from "./pages/MenuPage/MenuPage";
 import AsideMenu from "./components/AsideMenu/AsideMenu";
-import {useEffect} from "react";
+import { useEffect } from "react";
 import Slider from "./components/Slider/Slider";
 import Busket from "./components/Busket/Busket";
 import RegistrationForm from "./components/Header/RegistrationForm/RegistrationForm";
 import LoginForm from "./components/Header/LoginForm/LoginForm";
 import Сities from "./components/Header/Сities/Сities"
-import {Route, Router, Routes} from "react-router-dom";
+import { Route, Router, Routes } from "react-router-dom";
 
 
 function App() {
@@ -29,30 +29,48 @@ function App() {
 
     return (
         <div className={styles.App}>
-            <Header/>
+            <Header />
             {/*<Busket/>*/}
             <div className={styles.MainContent}>
                 <Routes>
                     <Route path={"/"} element={<div>
-                        <AsideMenu/>
-                        <Slider/>
-                    </div>}/>
+                        <AsideMenu />
+                        <Slider />
+                    </div>} />
+
                     <Route path={"/sasimi"} element={<div>
-                        <AsideMenu/>
-                        <MenuPage url='sasimi'/>
-                    </div>}/>
+                        <AsideMenu />
+                        <MenuPage url='sasimi' />
+                    </div>} />
+
                     <Route path={"/hot-rolls"} element={<div>
-                        <AsideMenu/>
-                        <MenuPage url='hot-rolls'/>
-                    </div>}/>
+                        <AsideMenu />
+                        <MenuPage url='hot-rolls' />
+                    </div>} />
+
                     <Route path={"/rolls"} element={<div>
-                        <AsideMenu/>
-                        <MenuPage url='rolls'/>
-                    </div>}/>
+                        <AsideMenu />
+                        <MenuPage url='rolls' />
+                    </div>} />
+
+                    <Route path={"/salats"} element={<div>
+                        <AsideMenu />
+                        <MenuPage url='salats' />
+                    </div>} />
+
+                    <Route path={"/sets"} element={<div>
+                        <AsideMenu />
+                        <MenuPage url='sets' />
+                    </div>} />
+
+                    <Route path={"/soups"} element={<div>
+                        <AsideMenu />
+                        <MenuPage url='soups' />
+                    </div>} />
                 </Routes>
 
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,15 +6,15 @@ function Header(props) {
         <section className={styles.Header}>
             <nav>
                 <ul>
-                    <li><img
-                        src="https://static.yakitoriya.ru/static/build/img/svg/yaki_logo.svg"
-                        alt="Якитория - доставка суши"/></li>
+                    <li>
+                        <img src="https://static.yakitoriya.ru/static/build/img/svg/yaki_logo.svg" alt="Якитория - доставка суши" />
+                    </li>
                     <li>Акции</li>
                     <li>Рестораны</li>
                     <li>О доставке</li>
-                    <li><span><a href="#"><img  className={styles.Point} src="https://w7.pngwing.com/pngs/239/38/png-transparent-computer-icons-location-others-angle-desktop-wallpaper-black.png" alt="Point"/></a></span><a href="#"><span className={styles.Header_region__active}>Москва и МО </span></a>
+                    <li><span><a href="#"><img className={styles.Point} src="https://w7.pngwing.com/pngs/239/38/png-transparent-computer-icons-location-others-angle-desktop-wallpaper-black.png" alt="Point" /></a></span><a href="#"><span className={styles.Header_region__active}>Москва и МО </span></a>
                     </li>
-                    <li><a href="#"><img className={styles.Login} src="https://avatars.mds.yandex.net/i?id=6915e065b1dadbdb3e33120d763b1890-4575500-images-thumbs&n=13" alt="Login"/> Login</a></li>
+                    <li><a href="#"><img className={styles.Login} src="https://avatars.mds.yandex.net/i?id=6915e065b1dadbdb3e33120d763b1890-4575500-images-thumbs&n=13" alt="Login" /> Login</a></li>
                     <li><a className={styles.Basket} href="#">Пустая корзина</a></li>
                 </ul>
             </nav>
