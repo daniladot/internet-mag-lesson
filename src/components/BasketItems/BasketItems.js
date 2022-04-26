97cf91a48cf9b2866b265a606e1314f5939d0cc7
Vasya990325 <vasya.arnaut.1999@mail.ru>
2022-04-26 13:56:54 +0300

    first commit

--- src/components/BasketItems/BasketItems.js
+++ src/components/BasketItems/BasketItems.js
@@ -4,9 +4,9 @@ import styles from './BasketItems.module.scss'
 function BasketItems(props) {
     return (
         <div className={styles.BasketItems}>
-            <p>{props.name}</p>
+            <p className={styles.start}>{props.name}</p>
             <p>{props.count}</p>
-            <p>{props.price * props.count}</p>
+            <p className={styles.end}>{props.price * props.count}<span className={styles.Rub}>&#8381;</span></p>
         </div>
     );
 }
